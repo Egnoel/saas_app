320ecee4f810705c181709c684deb9fa606fa013
Egnoel <70013986+Egnoel@users.noreply.github.com>
2025-08-10 22:09:09 +0100

    Implement Homepage Companion Card

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,43 @@
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
+import CTA from "@/components/CTA";
 
 
 const Page = () => {
   return (
-    <div>Page</div>
+    <main>
+        <h1 className={""}>Popular Companion</h1>
+        <section className={"home-section"}>
+            <CompanionCard
+            id={"123"}
+            name={"Neura the Brainy Explorer"}
+            topic={"Neural Network of the Brain"}
+            subject={"science"}
+            duration={45}
+            color={"#ffda6e"}
+            />
+            <CompanionCard
+                id={"456"}
+                name={"Countsy the Number Wizard"}
+                topic={"Derivatives & Integrals"}
+                subject={"Maths"}
+                duration={30}
+                color={"#FFDA6E"}
+            />
+            <CompanionCard
+                id={"789"}
+                name={"Verba the Vocabulary Builder"}
+                topic={"English Literature"}
+                subject={"Language"}
+                duration={30}
+                color={"#BDE7FF"}
+            />
+        </section>
+        <section className={"home-section"}>
+            <CompanionsList />
+            <CTA />
+        </section>
+    </main>
   )
 }
 
---
+++ components/CompanionsList.tsx
@@ -0,0 +1,11 @@
+
+
+const CompanionsList = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default CompanionsList;
